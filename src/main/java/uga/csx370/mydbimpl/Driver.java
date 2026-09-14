8409fd3e27dede6b492ea0d041448cba67e2d183
Aris Manglogiannis <asm07519@uga.edu>
2026-09-14 15:39:21 -0400

    Fix Aris Driver query

--- src/main/java/uga/csx370/mydbimpl/Driver.java
+++ src/main/java/uga/csx370/mydbimpl/Driver.java
@@ -99,13 +99,15 @@ public class Driver {
 	
 	Relation filtered2010 = RelationAlg.select(takes, row ->
 	    row.get(4).getAsInt() == 2010
-	    && (row.get(5).getAsString().equals("A") || row.get(5).getAsString().equals("A+")));
+	    && (row.get(5).getAsString().trim().equals("A")
+	        || row.get(5).getAsString().trim().equals("A+")));
 	Relation advisedStudents = RelationAlg.join(filtered2010, advisor, row ->
 	    row.get(0).getAsString().equals(row.get(6).getAsString()));
-	Relation renamedInstructorForEnglish = RelationAlg.rename(instructor, List.of("i_id"), List.of("inst_id"));
-	Relation englishAdvisors = RelationAlg.join(advisedStudents, renamedInstructorForEnglish);
+	Relation renamedInstructorForEnglish = RelationAlg.rename(instructor, List.of("i_id"), List.of("instructor_id"));
+	Relation englishAdvisors = RelationAlg.join(advisedStudents, renamedInstructorForEnglish, row ->
+	    row.get(7).getAsString().equals(String.valueOf(row.get(8).getAsInt())));
 	Relation englishTopStudents = RelationAlg.select(englishAdvisors, row ->
-	    row.get(9).getAsString().equals("English")
+	    row.get(10).getAsString().equals("English")
 	    && Integer.parseInt(row.get(0).getAsString()) < 50000);
 	Relation output_alex = RelationAlg.project(englishTopStudents,
 	    List.of("t_s_id", "course_id", "i_name", "i_dept_name", "i_salary"));
